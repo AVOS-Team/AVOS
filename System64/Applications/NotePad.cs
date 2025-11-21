8969a0cb31e21095e252d8ab72dd8e716566f238
Andrey Pepper <116492380+Andrey-Pepper@users.noreply.github.com>
2025-11-21 17:08:28 +0700

    Add files via upload

---
+++ System64/Applications/NotePad.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVOS.System64.Applications
+{
+    class NotePad
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Write the number of lines up to seventeen: ");
+            int t = Int32.Parse(Console.ReadLine());
+
+            switch (t)
+            {
+                case 1:
+                    Console.ReadLine();
+                    break;
+                case 2:
+                    Console.ReadLine();
+                    break;
+                case 3:
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    break;
+                case 4:
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    break;
+                case 5:
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    break;
+                case 6:
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    break;
+                case 7:
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    break;
+                case 8:
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    break;
+                case 9:
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    break;
+                case 10:
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    break;
+
+
+                case 11:
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    break;
+
+                case 12:
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    break;
+
+                case 13:
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    break;
+
+                case 14:
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    break;
+
+                case 15:
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    break;
+
+                case 16:
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    break;
+
+                case 17:
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    Console.ReadLine();
+                    break;
+            }
+
+        }
+    }
+}
