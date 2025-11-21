3c61169df2f9210eccb608ad0122c855636a7e50
Andrey Pepper <116492380+Andrey-Pepper@users.noreply.github.com>
2025-11-21 16:51:11 +0700

    Add files via upload

---
+++ Configuration/Config.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVOS.System64.Configuration
+{
+    public class Config
+    {
+        private Dictionary<string, string> configuration;
+        private string _filepath;
+        public Config(string filepath)
+        {
+            string[] lines = File.ReadAllLines(filepath);
+            configuration = new Dictionary<string, string>();
+            foreach (string line in lines)
+                configuration.Add(line.Split('=')[0], line.Split('=')[1]);
+            _filepath = filepath;
+        }
+        public string this[string ConfLine]
+        {
+            get => configuration[ConfLine];
+            set => configuration[ConfLine] = value;
+        }
+        public string[] GetConfigEntries() => configuration.Keys.ToArray();
+        public string GetValue(string key) => configuration[key];
+        public void SetValue(string key, string value) => configuration[key] = value;
+        public void AddPair(string key, string value) => configuration.Add(key, value);
+        public void RemovePair(string key) => configuration.Remove(key);
+        public void Save()
+        {
+            List<string> conf = new List<string>();
+            foreach (KeyValuePair<string, string> pair in configuration)
+                conf.Add($"{pair.Key}={pair.Value}");
+            File.WriteAllLines(_filepath, conf.ToArray());
+        }
+    }
+}
---
+++ Graphics/FPSCounter.cs
@@ -0,0 +1,31 @@
+﻿using Cosmos.HAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVOS.System64.Graphics
+{
+    public class FPSCounter
+    {
+        private int frames = 0;
+        private uint lastSecond = 0;
+        private int fps = 0;
+
+        public int FPS => fps;
+
+        public void FrameUpdate()
+        {
+            frames++;
+            uint currentSecond = RTC.Second;
+            if (currentSecond != lastSecond)
+            {
+                fps = frames;
+                frames = 0;
+                lastSecond = currentSecond;
+            }
+        }
+    }
+
+}
