1b9a9500381771ceb7db401cf3fdfda31b112635
Andrey Pepper <116492380+Andrey-Pepper@users.noreply.github.com>
2025-12-04 13:50:55 +0700

    ✨Add ftp support

---
+++ System64/Drivers/Network/FTP/FTP.cs
@@ -0,0 +1,22 @@
+﻿using Cosmos.System.Network.Config;
+using CosmosFtpServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVOS.System64.Drivers.Network.FTP
+{
+    class FTP
+    {
+        public static void Start()
+        {
+            using (var xServer = new FtpServer(Kernel.fs, @"0:\\"))
+            {
+                Console.WriteLine("FTP Server listening at " + NetworkConfiguration.CurrentAddress + ":21 ...");
+                xServer.Listen();
+            }
+        }
+    }
+}
